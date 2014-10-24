337c966d8e70140c97af2d4fb1a711720c8ad31b
Brian Jacobel <bjacobel@gmail.com>
2014-10-24 00:38:33 -0400

    Change the name of the bucket because it's under a new account now

--- backup.sh
+++ backup.sh
@@ -9,10 +9,10 @@ source /home/orientweb/bowdoinorient.com/htdocs/python-env/bin/activate
 mysqldump -u$DBUSER -p$DBPW $DBDB | gzip > $DBDB-$DATE.sql.gz
 
 # Rsync the database to S3
-boto-rsync -a $AWS_BONUS_ACCESS_KEY -s $AWS_BONUS_SECRET_KEY $DBDB-$DATE.sql.gz s3://orient-backup
+boto-rsync -a $AWS_BONUS_ACCESS_KEY -s $AWS_BONUS_SECRET_KEY $DBDB-$DATE.sql.gz s3://bowdoinorient-bonus
 
 # Remove the local backup
 rm $DBDB-$DATE.sql.gz
 
 # Rsync the images to S3 as well
-boto-rsync -a $AWS_BONUS_ACCESS_KEY -s $AWS_BONUS_SECRET_KEY images/ s3://orient-backup/images
+boto-rsync -a $AWS_BONUS_ACCESS_KEY -s $AWS_BONUS_SECRET_KEY images/ s3://bowdoinorient-bonus/images
