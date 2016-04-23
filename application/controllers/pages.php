0197badc34f2ed92e1886aea4a92774549451782
Brian Jacobel <bjacobel@gmail.com>
2016-04-23 18:56:38 -0400

    Update isitiviesyet.com with Ivies dates for 2016

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -9,12 +9,12 @@ class Pages extends CI_Controller {
         $this->load->model('attachments_model', '', TRUE);
         $this->load->model('pages_model', '', TRUE);
     }
-    
+
     public function index()
     {
         $this->load->view('welcome_message');
     }
-    
+
     public function error()
     {
         // meta
@@ -23,10 +23,10 @@ class Pages extends CI_Controller {
         $data->page_title = 'Page not found — The Bowdoin Orient';
         $data->page_description = '';
         $data->page_type = '';
-        
+
         $this->load->view('error');
     }
-    
+
     public function view($page)
     {
         // check if requested page exists, load if it does
@@ -36,7 +36,7 @@ class Pages extends CI_Controller {
             $data->footerdata = new stdClass();
             $data->headerdata = new stdClass();
 
-            $data->page_title = 'The Bowdoin Orient';            
+            $data->page_title = 'The Bowdoin Orient';
             $page_title = array(
                 'about'            => 'About',
                 'advertise'        => 'Advertise',
@@ -51,7 +51,7 @@ class Pages extends CI_Controller {
                 'apply'        => 'Bowdoin Orient Application'
                 );
             if(isset($page_title[$page])) $data->page_title = $page_title[$page].' — The Bowdoin Orient';
-            
+
             $data->page = $page;
 
             // Basically using content as a catch all for anything I want to
@@ -62,12 +62,12 @@ class Pages extends CI_Controller {
             $data->headerdata->date = date("Y-m-d");
             $this->load->view('pages/'.$page, $data);
         }
-        else 
+        else
         {
             $this->error();
         }
-    }    
-    
+    }
+
     public function search()
     {
         $data = new stdClass();
@@ -77,16 +77,16 @@ class Pages extends CI_Controller {
         $data->query = $this->input->get("q");
         $data->footerdata->quote = $this->attachments_model->get_random_quote();
         $data->headerdata->date = date("Y-m-d");
-        
+
         // meta
         if($data->query) { $data->page_title = 'Search results for "'.$data->query.'" — The Bowdoin Orient'; }
         else { $data->page_title = 'Search — The Bowdoin Orient'; }
         $data->page_description = '';
         $data->page_type = '';
-        
+
         $this->load->view('search', $data);
     }
-    
+
     public function advsearch()
     {
         $data = new stdClass();
@@ -102,24 +102,24 @@ class Pages extends CI_Controller {
             $data->searchdata = $searchdata;
             $data->articles = $this->article_model->advsearch($this->input->get());
         }
-        
+
         $this->load->helper('form');
         $data->footerdata = new stdClass();
         $data->headerdata = new stdClass();
         $data->footerdata->quote = $this->attachments_model->get_random_quote();
         $data->headerdata->date = date("Y-m-d");
-        
+
         // meta
         $data->page_title = 'Advanced search — The Bowdoin Orient';
         $data->page_description = '';
         $data->page_type = '';
-        
+
         $this->load->view('advsearch', $data);
     }
-    
+
     public function phpinfo()
     {
-        if(!bonus()) 
+        if(!bonus())
         {
             $this->load->view('error');
         }
@@ -128,24 +128,24 @@ class Pages extends CI_Controller {
             exit(phpinfo());
         }
     }
-    
+
     public function isMobile()
     {
         echo (isMobile() ? "Yes, we think you're browsing on a mobile device." : "No, we don't think you're browsing on a mobile device.");
         exit(" If this is incorrect, email andrew.daniels714@gmail.com, including information on what browser and operating system you're using.");
     }
-    
+
     public function isitivies()
     {
-        if(time() < strtotime("2015-04-20 00:00:00")) $message = "No";
-        else if(time() < strtotime("2015-04-22 00:00:00")) $message = "Basically";
-        else if(time() < strtotime("2015-04-26 00:00:00")) $message = "Yes";
-        else if(time() < strtotime("2015-04-27 00:00:00")) $message = "Not really";
-        else if(time() < strtotime("2015-04-28 00:00:00")) $message = "You wish";
+        if(time() < strtotime("2016-04-25 00:00:00")) $message = "No";
+        else if(time() < strtotime("2016-04-27 00:00:00")) $message = "Basically";
+        else if(time() < strtotime("2016-05-01 00:00:00")) $message = "Yes";
+        else if(time() < strtotime("2016-05-02 00:00:00")) $message = "Not really";
+        else if(time() < strtotime("2016-05-03 00:00:00")) $message = "You wish";
         else $message = "No";
-                
+
         $data = new stdClass();
-        
+
         $data->message = $message;
         $this->load->view('isitivies',$data);
     }
